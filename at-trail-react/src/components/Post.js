dea4291c366b9e224de67680ab4145d298d59c7c
David Turk <dlturk747@gmail.com>
2017-08-25 14:46:16 -0400

    Posts with no photos are now displayed

--- at-trail-react/src/components/Post.js
+++ at-trail-react/src/components/Post.js
@@ -2,11 +2,23 @@ import React from 'react';
 
 const Post = (props) => {
   const postItems = props.posts.map( (post) => {
+    //Logic to create a post
+    let text = post.text;
+    let photos = post.photos;
+    let postContent = [];
+
+    if(photos.length){
+      postContent.push(<p>This is a text</p>)
+    }
+    else {
+      postContent.push(<p>{text}</p>);
+    }
+
     return (
       <div className="post">
         <div className="date"></div>
         <div className="body">
-          <p>{post.text}</p>
+          {postContent}
         </div>
       </div>
     )
